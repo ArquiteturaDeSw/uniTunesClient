f1f13337fccff6e8ccd64efc12a68ccf9d78ddb4
Wesley <wesleys.felipe@gmail.com>
2016-06-05 16:14:11 -0300

    WIP | User Profile

--- app/user/user.js
+++ app/user/user.js
@@ -3,9 +3,14 @@
 angular.module('uniTunesApp.User', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/user/cadastro', {
+  $routeProvider.when('/cadastro', {
     templateUrl: 'user/new-user.html',
     controller: 'UserCtrl'
+  })
+
+  .when('/usuario/perfil', {
+      templateUrl: 'user/user.html',
+      controller: 'UserCtrl'
   });
 }])
 
